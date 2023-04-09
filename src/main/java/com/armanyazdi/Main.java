8d0cdff59d9b7c1a32b31a1b7483aab168b13684
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-10 01:46:15 +0330

    Update Main.java

--- src/main/java/com/armanyazdi/Main.java
+++ src/main/java/com/armanyazdi/Main.java
@@ -1,7 +1,18 @@
 package com.armanyazdi;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        SleepCalculator.sleepCalcultor();
+        System.out.println(ConsoleColors.YELLOW + "1) Calculate bedtime");
+        System.out.println("2) Calculate wake-up time" + ConsoleColors.RESET);
+        System.out.print("Enter your choice: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+
+        if (choice.equals("1")) SleepCalculator.bedTime();
+        else if (choice.equals("2")) SleepCalculator.wakeUpTime();
+        else System.out.println(ConsoleColors.RED_BOLD + "Exception: Invalid input!" + ConsoleColors.RESET);
     }
 }
